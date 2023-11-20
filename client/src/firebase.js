0b36a1d2983632e9d8fd7d2f410bbdf3f53680fc
Demilade111 <demiladealuko111@gmail.com>
2023-11-20 22:48:19 +0100

    first commit

---
+++ client/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD1UpIL-IM_yiwVGA8TBZQmopbPrbXAm9s",
+  authDomain: "real-estate-3232d.firebaseapp.com",
+  projectId: "real-estate-3232d",
+  storageBucket: "real-estate-3232d.appspot.com",
+  messagingSenderId: "1088121864904",
+  appId: "1:1088121864904:web:ae61dd82c152dc5a7e1bfd",
+  measurementId: "G-CRNDH08KJV",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
